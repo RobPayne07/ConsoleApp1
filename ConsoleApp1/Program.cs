038864ff79c44338673cfb3a0700f49191278b21
Rob Payne <rkpayne07@gmail.com>
2018-12-09 21:51:22 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool valInt = false;
+            Console.WriteLine("Header");
+
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out int k))
+                {
+                    valInt = false;
+                }
+                else
+                {
+                    Console.WriteLine("Well done, you're not a fuckup.");
+                    valInt = true;
+                }
+
+            } while (!valInt);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
